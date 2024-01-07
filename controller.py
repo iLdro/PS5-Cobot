1e515e999a613069e5231861ba63239cfcabd5b7
jukle <julien.kle02@gmail.com>
2024-01-07 11:46:23 +0100

    Merge branch 'main' of https://github.com/iLdro/PS5-Cobot

---
+++ controller.py
@@ -0,0 +1,42 @@
+from inputs import get_gamepad
+import math
+import threading
+
+class XboxController(object):
+    MAX_TRIG_VAL = math.pow(2, 8)
+    MAX_JOY_VAL = math.pow(2, 15)
+
+    def __init__(self):
+
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+    
+
+        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
+
+
+    def read(self): # return the buttons/triggers that you care about in this methode
+        x = self.LeftJoystickX
+        y = self.LeftJoystickY
+     
+        return [x, y]
+
+
+    def _monitor_controller(self):
+        while True:
+            events = get_gamepad()
+            for event in events:
+                if event.code == 'ABS_Y':
+                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_X':
+                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+            
+
+
+
+if __name__ == '__main__':
+    joy = XboxController()
+    while True:
+        print(joy.read())
